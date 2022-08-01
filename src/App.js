71a3b02fc701d84cfd7ed9ac7b639a18421fe463
katmart93 <katwlodarczyk93@gmail.com>
2022-08-01 15:25:05 +0200

    todos displayed on the page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+
+    setTodos([
+      ...todos,
+      {
+        text: inputValue,
+        id: uuidv4(),
+      },
+    ]);
+  };
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="container">
+        <form onSubmit={addTodo}>
+          <input
+            type="text"
+            placeholder="Add a task..."
+            value={inputValue}
+            onChange={(e) => {
+              setInputValue(e.target.value);
+            }}
+          />
+          <button type="submit">Add</button>
+        </form>
+        {todos.map((todo) => (
+          <div key={todo.id} className="todo">
+            <p>{todo.text}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
