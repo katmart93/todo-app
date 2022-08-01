257e328b1b3e0447211b211f24c2abac4290ec10
katmart93 <katwlodarczyk93@gmail.com>
2022-08-01 21:32:30 +0200

    local-storage not working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { useState, useEffect, useRef } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
+import "./App.css";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
@@ -8,7 +8,7 @@ function App() {
 
   const addTodo = (e) => {
     e.preventDefault();
-
+    if (inputValue.trim() === "") return;
     setTodos([
       ...todos,
       {
@@ -16,13 +16,29 @@ function App() {
         id: uuidv4(),
       },
     ]);
+
+    setInputValue("");
   };
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  useEffect(() => {
+    const localData = localStorage.getItem("TODO_APP");
+    if (localData !== null) setTodos(JSON.parse(localData));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("TODO_APP", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <div className="App">
       <div className="container">
         <form onSubmit={addTodo}>
           <input
+            autoFocus
             type="text"
             placeholder="Add a task..."
             value={inputValue}
@@ -35,6 +51,10 @@ function App() {
         {todos.map((todo) => (
           <div key={todo.id} className="todo">
             <p>{todo.text}</p>
+            <i
+              onClick={() => removeTodo(todo.id)}
+              className="fa-regular fa-trash-can"
+            ></i>
           </div>
         ))}
       </div>
